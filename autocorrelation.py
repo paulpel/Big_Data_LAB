a262ad0a3681359dcd6505b1b7b7749b1584d533
Paul <pawelpelar@gmail.com>
2021-04-03 22:42:32 +0200

    Visualization preset

--- autocorrelation.py
+++ autocorrelation.py
@@ -10,17 +10,21 @@ class Autocorrelation:
         self.time_delatation = 500
         self.arguments_t = np.linspace(-1,1,self.number_of_probes)
         self.signal = np.sin(np.pi * self.arguments_t)
+        self.results =[]
 
     def autocorrelation(self):
-        values = []
         for r in range(self.time_delatation):
             temp = 0
             for i in range(1,self.number_of_probes-r):
                 temp += self.signal[i] * self.signal[i-r]
             temp = (1/(self.number_of_probes-r))*temp
-            values.append(temp)
+            self.results.append(temp)
 
-        return values
+    def visualization(self):
+        ''''''
+        plt.plot(self.results)
+        plt.show()
 
 au = Autocorrelation()
-print(au.autocorrelation()[-1])
+au.autocorrelation()
+au.visualization()
